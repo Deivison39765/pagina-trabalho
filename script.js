7275d77b9b84c3ff112ca16b0e4e3eb347afe122
Deivison Souza <deivisonsouza200@gmail.com>
2025-07-07 22:03:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("meuBotao").addEventListener("click", function() {
+  alert("Seja bem-vindo à minha página! É um prazer ter você aqui. Aproveite o conteúdo e fique à vontade!");
+});
